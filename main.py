8c9a282cd0890da674d872cb147d831ef4bb3529
Dan Serino <serino.daniel@gmail.com>
2022-01-26 14:00:19 -0500

    clean up

--- main.py
+++ main.py
@@ -1,18 +1,33 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-# notes:
-# go to a higher N (for example 100)
+"""
+Represent an image using a 2D Fourier series
+
+full color image animation:
+ffmpeg -framerate 10 -i fourier_%d.png -c:v libx264 -vf "fps=60" -crf 10 -c:a aac fourier.mp4
+
+rgb images animation:
+ffmpeg -framerate 10 -i fourier_combined_%d.png -c:v libx264 -vf "fps=60" -crf 10 -c:a aac fourier_combined.mp4
+
+fourier mode animation:
+ffmpeg -framerate 2 -start_number 0 -i anim_%d.png -t 0:12.5 -c:v libx264 -vf "fps=60" -crf 10 -c:a aac anim1.mp4
+ffmpeg -framerate 14 -start_number 25 -i anim_%d.png -t 0:4.0 -c:v libx264 -vf "fps=60" -crf 10 -c:a aac anim2.mp4
+ffmpeg -framerate 44 -start_number 81 -i anim_%d.png -t 0:2.0 -c:v libx264 -vf "fps=60" -crf 10 -c:a aac anim3.mp4
+ffmpeg -framerate 60 -start_number 169 -i anim_%d.png -c:v libx264 -vf "fps=60" -crf 10 -c:a aac anim4.mp4
+"""
 
 
-do_save = True
+do_save = False
 do_anim = False
-start_frames = 20
-middle_frames = 50
-end_frames = 20
 
 
 def plot_coeffs(mode, coeffs, img, anim_count, fig_no=3, name=None, compression=None, N=None, k=0, l=0, center=None):
+    # 4 plots side-by-side
+    # plot 1: fourier mode in grayscale
+    # plot 2: absolute value of fourier coefficients (transformed by sqrt function)
+    # plot 3: complex angle of the fourier coeffients
+    # plot 4: image reconstruction
     
     fig = plt.figure(fig_no,
                      dpi=240,
@@ -57,6 +72,9 @@ def plot_coeffs(mode, coeffs, img, anim_count, fig_no=3, name=None, compression=
 
 
 def plot_all(img, fig_no=1, name=None, label=None):
+    # two plots
+    # plot 1: (red, green, blue, full) image reconstructions
+    # plot 2: full image reconstruction
     
     img_r = img+0
     img_g = img+0
@@ -119,6 +137,9 @@ def plot_all(img, fig_no=1, name=None, label=None):
 
 
 def square_boundary(N):
+    # give integer pairs representing the
+    # coordinates of boundary of the square defined by
+    # vertices (N, N), (-N, N), (N, -N), (-N, -N)
 
     i = N
     j = N
@@ -145,43 +166,59 @@ def square_boundary(N):
     
 
 def main():
+    # number of modes to use
     N = 100
-    # N = 3
-    anim_count = 0
-    
+
+    # original image
     img_dir = 'pikachu.jpg'
 
+    # plot image
     img = plt.imread(img_dir)
     plot_all(img, fig_no=1, name="figs/og*")
 
+    # get image shape and size
     npx, npy, d = np.shape(img)
-
     total_pixels = npx*npy
 
+    # create a mesh for the pixels
     xv = np.linspace(0, npx, npx+1) / npx
     yv = np.linspace(0, npy, npy+1) / npy
-    val = np.zeros((npx, npy, 3)) 
-    prev_val = val+0
+
+    # reconstruction of image
+    val = np.zeros((npx, npy, 3))
+    # fourier mode
     mode = np.zeros((npx, npy, 3))
-    
+    # fourier coefficients
     coeffs = np.zeros((2*N+1, 2*N+1, 3), dtype=complex)
     center = N
-    
+
+    # half points of pixel squares
     xvh = 0.5 * (xv[+1:]+xv[:-1])
     yvh = 0.5 * (yv[+1:]+yv[:-1])
-    
+
+    # grid spacing
     dx = 1 / npx
     dy = 1 / npy
 
     count = 0
+    anim_count = 0
 
     # loop through all combos of k and l
     av = [[], [], []]
     for n in range(N+1):
+        # get integer pairs on the square defined by n
         pairs = square_boundary(n)
+        # loop through each pair and compute fourier coefficient
         for l, k in pairs:
             print((k, l), flush=True)
-            
+
+            # term: is the fourier coefficient
+            # the (k, l)'th coefficient is given by
+            # 1 / (Lx Ly) \int_{0}^{Lx} \int_{0}^{Ly} c(x, y) exp(-2 pi i k x / Lx) exp(-2 pi i l y / Ly) dx dy
+            # where c(x, y) is the image.
+            # This is computed exactly using the fact that images are constant over a pixel domain.
+            # (This could be probably be sped up significantly using the DFT.)
+
             hv = (+ np.exp(- 2 * np.pi * 1j * l * yv[+1:])
                   - np.exp(- 2 * np.pi * 1j * l * yv[:-1]))
             gv = (+ np.exp(- 2 * np.pi * 1j * k * xv[+1:])
@@ -191,24 +228,21 @@ def main():
             pv = np.exp(2 * np.pi * 1j * k * xvh)
             pv_qv = np.outer(pv, qv)
 
+            # compute for r, g, and b
             for dim in range(3):
                 if k == 0:
                     if l == 0:
                         term = dx * dy * np.sum( img[:, :, dim] )
                     else:
                         term = - dx / (2 * np.pi * 1j * l) \
-                            * np.sum( img[:, :, dim] \
-                                      *  hv )
+                            * np.sum( img[:, :, dim] *  hv )
                 else:
                     if l == 0:
                         term = - dy / (2 * np.pi * 1j * k) \
-                            * np.sum( img[:, :, dim].T \
-                                      * gv )
+                            * np.sum( img[:, :, dim].T * gv )
                     else:
                         term = - 1 / (4 * np.pi ** 2 * k * l) \
-                            * np.sum( (img[:, :, dim] \
-                                       * hv).T
-                                      * gv )
+                            * np.sum( (img[:, :, dim] * hv).T * gv )
 
                 av[dim].append(term)
 
@@ -221,6 +255,7 @@ def main():
 
             count += 1
 
+            # plot
             name = "figs/coeffs_%d" % (count)
             compression = 2*count / total_pixels
             if do_anim:
@@ -234,70 +269,15 @@ def main():
                 for dim in range(3):
                     mode[:, :, dim] = start
 
-                # plot_val = alpha*new_val+(1-alpha)*prev_val
                 plot_val = new_val
                 plot_coeffs(mode, coeffs / 255, plot_val, anim_count,
                             fig_no=3, compression=compression, N=n, k=k, l=l, center=center)
                 anim_count += 1
-
-                # update
-                prev_val = new_val
 
         # plot
         name = "figs/fourier*_%d" % (n)
         plot_all(np.clip(val, 0, 1), fig_no=2, name=name, label=n)
 
 
-    # plt.figure()
-    # plt.plot(np.abs(np.array(av)))
-    
-    # for n in range(N+1):
-    #     pairs = square_boundary(n)
-    #     for l, k in pairs:
-    #         for dim in range(3):
-        
-
-    # plt.show()
-
-# ffmpeg -y -i fourier_%d.png -vf "fps=60,scale=3840:-1:flags=lanczos,split[s0][s1];[s0]palettegen[p];[s1][p]paletteuse" -loop 0 fourier.gif
-# ffmpeg -y -i fourier_combined_%d.png -vf "fps=60,scale=3840:-1:flags=lanczos,split[s0][s1];[s0]palettegen[p];[s1][p]paletteuse" -loop 0 fourier_combined.gif
-# ffmpeg -framerate 60 -i coeffs_%d.png -r 60 -vf scale=3840:-1 coeffs.gif
-
-# figures
-# ffmpeg -framerate 10 -i fourier_%d.png -c:v libx264 -vf "fps=60" -crf 10 -c:a aac fourier.mp4
-# ffmpeg -framerate 10 -i fourier_combined_%d.png -c:v libx264 -vf "fps=60" -crf 10 -c:a aac fourier_combined.mp4
-
-
-# animation:
-# ffmpeg -framerate 2 -start_number 0 -i anim_%d.png -t 0:12.5 -c:v libx264 -vf "fps=60" -crf 10 -c:a aac anim1.mp4
-# ffmpeg -framerate 14 -start_number 25 -i anim_%d.png -t 0:4.0 -c:v libx264 -vf "fps=60" -crf 10 -c:a aac anim2.mp4
-# ffmpeg -framerate 44 -start_number 81 -i anim_%d.png -t 0:2.0 -c:v libx264 -vf "fps=60" -crf 10 -c:a aac anim3.mp4
-# ffmpeg -framerate 60 -start_number 169 -i anim_%d.png -c:v libx264 -vf "fps=60" -crf 10 -c:a aac anim4.mp4
-
-def cmds():
-    start = 0
-    fr = 2
-
-    count = 1
-    for N in range(3, 10, 2):
-        stop_number = (2*N-1)**2
-
-        t = (stop_number - start) / fr
-
-        if N == 9:
-            print('ffmpeg -framerate %d -start_number %d -i anim_%%d.png -c:v libx264 -vf "fps=60" -crf 10 -c:a aac anim%d.mp4' % (fr, start, count))
-        else:
-            print('ffmpeg -framerate %d -start_number %d -i anim_%%d.png -t 0:%.1f -c:v libx264 -vf "fps=60" -crf 10 -c:a aac anim%d.mp4' % (fr, start, t, count))
-
-        count += 1
-        if N == 3:
-            x = 2
-        else:
-            x = 4
-            
-        fr = min(x*(2*(N+2)-3), 60)
-        start = stop_number
-
 if __name__ == "__main__":
     main()
-    # cmds()
